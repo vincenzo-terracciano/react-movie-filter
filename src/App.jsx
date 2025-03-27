1af80ae545e6f8a685b0db3b999a74b653f05a1d
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-27 17:29:01 +0100

    Add search functionality for movie titles in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,59 @@ function App() {
 
   const [selectedGenre, setSelectedGenre] = useState('')
   const [filteredMovies, setFilteredMovies] = useState(movies)
+  const [search, setSearch] = useState('')
+  const [newMovies, setNewMovies] = useState('')
+
 
   useEffect(() => {
     setFilteredMovies(movies.filter(
-      movie => movie.genre.toLowerCase().includes(selectedGenre.toLocaleLowerCase())))
+      movie => movie.genre.toLowerCase().includes(selectedGenre.toLowerCase())))
   }, [selectedGenre])
+
+  useEffect(() => {
+    setFilteredMovies(movies.filter(
+      movie => movie.title.toLowerCase().includes(search.toLowerCase())
+    ))
+  }, [search])
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(search);
+
+    setSearch('')
+  }
 
   return (
     <>
       <h1 className='d-flex justify-content-center'>Movies</h1>
 
       <div className="container">
-        <select onChange={e => setSelectedGenre(e.target.value)} value={selectedGenre} className='my-4'>
+        <select onChange={e => setSelectedGenre(e.target.value)} value={selectedGenre}
+          className='my-4'>
           <option value="">Tutti i generi</option>
           <option value="Fantascienza">Fantascienza</option>
           <option value="Thriller">Thriller</option>
           <option value="Romantico">Romantico</option>
           <option value="Azione">Azione</option>
         </select>
+
+        <form onSubmit={handleFormSubmit}>
+          <div className="mb-3">
+            <label htmlFor="new-title" className="form-label">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              name="new-title"
+              id="new-title"
+              aria-describedby="helpTitle"
+              placeholder="Type a film"
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+            />
+            <button className='btn btn-primary my-3' type="submit">Search</button>
+          </div>
+
+        </form>
 
         <div className="card">
           <div className="card-body">
@@ -35,6 +70,8 @@ function App() {
             </div>
           </div>
         </div >
+
+
       </div>
 
     </>
