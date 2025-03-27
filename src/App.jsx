1e258d131fb36ee7fef498253216e27a9b6beceb
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-27 15:31:57 +0100

    Add Bootstrap for styling and enhance movie display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ function App() {
 
   return (
     <>
+      <h1 className='d-flex justify-content-center'>Movies</h1>
 
+      <div className="card">
+        <div className="card-body">
+          <div>
+            {
+              movies.map(movie => (
+                <h5>{movie.title}, {movie.genre} </h5>
+              ))
+            }
+          </div>
+        </div>
+      </div >
     </>
   )
 }
