fa4d0b58e1f3e7bdefc3f14e8e2b3f6dd7c7d82a
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-27 16:43:30 +0100

    Implement genre filtering for movie display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import movies from './data/movies'
 
 function App() {
+
+  const [selectedGenre, setSelectedGenre] = useState('')
+  const [filteredMovies, setFilteredMovies] = useState(movies)
+
+  useEffect(() => {
+    setFilteredMovies(movies.filter(
+      movie => movie.genre.toLowerCase().includes(selectedGenre.toLocaleLowerCase())))
+  }, [selectedGenre])
 
   return (
     <>
       <h1 className='d-flex justify-content-center'>Movies</h1>
 
-      <div className="card">
-        <div className="card-body">
-          <div>
-            {
-              movies.map(movie => (
-                <h5>{movie.title}, {movie.genre} </h5>
-              ))
-            }
+      <div className="container">
+        <select onChange={e => setSelectedGenre(e.target.value)} value={selectedGenre} className='my-4'>
+          <option value="">Tutti i generi</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+
+        <div className="card">
+          <div className="card-body">
+            <div>
+              {
+                filteredMovies.map((movie, index) => (
+                  <h5 key={index}>{movie.title}, {movie.genre} </h5>
+                ))
+              }
+            </div>
           </div>
-        </div>
-      </div >
+        </div >
+      </div>
+
     </>
   )
 }
