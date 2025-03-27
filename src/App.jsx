79db12322d12248f7954bc9b9cc6ab493fd6970f
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-27 15:12:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import movies from './data/movies'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/data/movies.js
@@ -0,0 +1,28 @@
+const movies = [
+    {
+        title: 'Inception',
+        genre: 'Fantascienza'
+    },
+    {
+        title: 'Il Padrino',
+        genre: 'Thriller'
+    },
+    {
+        title: 'Titanic',
+        genre: 'Romantico'
+    },
+    {
+        title: 'Batman',
+        genre: 'Azione'
+    },
+    {
+        title: 'Interstellar',
+        genre: 'Fantascienza'
+    },
+    {
+        title: 'Pulp Fiction',
+        genre: 'Thriller'
+    },
+]
+
+export default movies
